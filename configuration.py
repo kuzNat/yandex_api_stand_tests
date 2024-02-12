f22a94f7e89e011fceee8afb6e142f1decd74bcf
natKuz <kuznetsova-pgups@yandex.ru>
2024-02-12 20:44:58 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://c7758a24-26c0-4d6d-98ea-b4f47dfd008c.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
